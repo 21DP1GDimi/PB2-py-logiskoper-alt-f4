6dc38978e3ab35cc8762638a600c2aaa78ef7726
21DP1GDimi <106299275+21DP1GDimi@users.noreply.github.com>
2022-09-25 18:07:37 +0300

    Update pirmais.py

--- pirmais.py
+++ pirmais.py
@@ -1,17 +1,21 @@
-liet_x = float(input("x:"))
-liet_y = float(input("y:"))
+"""
+Programma, kas nosaka, kur atrodas punkts, figūras iekšā, uz līnijas vai ārā
 
-if liet_x > -1 and liet_x < 3:
-    if liet_y < 1 and liet_y > -2:
+Izveidoja: Gļebs Dimitrijevs DP2-1
+"""
+
+def nosaka_punktu(liet_x, liet_y):
+    
+    # Ievadam lietotāja punkta datus 
+
+    # Salīdzinam vērtības, meklējam puntkta atrašanas vietu
+    if liet_x > -1 and liet_x < 3 and liet_y < 1 and liet_y > -2:
         print("iekšā")
-    elif liet_y == 1 or liet_y == -2:
-        print("uz līnijas")
-    else:
-        print("ārā")
-elif liet_x == 3 or liet_x == -1:
-    if liet_y < 1 and liet_y > -2:
+    elif liet_x >= -1 and liet_x <= 3 and liet_y == 1 or liet_y == -2 or liet_y <= 1 and liet_y >= -2 and liet_x == -1 or liet_x == 3:
         print("uz linijas")
     else:
         print("ārā")
-else:
-    print("ārā")
+        
+
+# Izsaucām programmu, ievadam lietotāja punkta datus 
+nosaka_punktu(liet_x = float(input("x:")), liet_y = float(input("y:")))
